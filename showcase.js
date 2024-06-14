de6abbbadc61ae17f46aa1325a3a10c344c454f0
RainierRoss <rainierrossi@live.wsd1.org>
2024-06-14 09:04:38 -0500

    DFG

---
+++ showcase.js
@@ -0,0 +1,46 @@
+const menuIcon = document.querySelector(".menu-icon");
+//grab outer div class navbar
+const navbar = document.querySelector(".navbar");
+
+//when you click on the hamburger
+menuIcon.addEventListener("click", () => {
+    //adds and removes toggle to the navbar class div
+    //effectively showing and hiding the navigation screen
+navbar.classList.toggle("change");
+console.log("hello")
+})
+ 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("images");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+        
+    
+      
+
+    
+  
